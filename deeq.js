baef3f0287f1ac8e4ee0985e72d1ceac20a658a7
Abdirisak <Abdirisakabiib69@gmail.com>
2022-10-15 22:52:55 +0300

    asc

---
+++ deeq.js
@@ -0,0 +1,13 @@
+var grade
+var checkgrade
+grade=window.prompt("enter your grade") 
+chechgrade-parseInt(grade)
+
+if(checkgrade>90)
+{
+	console.log("A")
+}
+else if(checkgrade>80)
+{
+	console.log("B")
+}
